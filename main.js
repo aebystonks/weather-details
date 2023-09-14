ddfdbdca372fd9ed8e380cdc9f740aef29431aeb
intranetdwaight <grimesjohnnn@gmail.com>
2023-09-15 00:01:13 +0600

    first commit

--- main.js
+++ main.js
@@ -6,11 +6,15 @@ const weatherImg = document.querySelector('.weather-img')
 const feelsLike = document.querySelector('#feels')
 const sunriseInfo = document.querySelector('#sunrise')
 const sunsetInfo = document.querySelector('#sunset')
-const time = document.querySelectorAll('.time')
+const container = document.querySelectorAll('.hourly-item')
+const favouriteButton = document.querySelector('.favourite')
 
-for (let i = 0; i < time.length; i++) {
-	console.log(time[i])
-}
+const time = document.querySelectorAll('.time')
+const forecastTemp = document.querySelectorAll('.temperature')
+const forecastFeelsLike = document.querySelectorAll('.feels-like')
+const hourlyIcon = document.querySelectorAll('.hourly-weather-img')
+
+let citiesList = []
 
 async function getCity(cityName) {
 	const serverUrl = 'http://api.openweathermap.org/data/2.5/weather'
@@ -21,16 +25,48 @@ async function getCity(cityName) {
 	return data
 }
 
-async function getForecast(cityName) {
+async function fetchWeatherData(cityName) {
+	const forecast = 'https://api.openweathermap.org/data/2.5/forecast'
+	const apiKey = '1fc1c3c4f7ab985357be46392f09aafe'
+	const url = `${forecast}?q=${cityName}&appid=${apiKey}&units=metric`
 	try {
-		const forecast = 'https://api.openweathermap.org/data/2.5/forecast'
-		const apiKey = '1fc1c3c4f7ab985357be46392f09aafe'
-		const url = `${forecast}?q=${cityName}&appid=${apiKey}&units=metric`
 		const forecastResponse = await fetch(url)
-		const forecastData = await forecastResponse.json()
-		return forecastData
-	} catch {
-		throw new Error('404: Not Found!')
+		return await forecastResponse.json()
+	} catch (error) {
+		console.error('Error fetching data:', error)
+		throw error
+	}
+}
+
+function formatTime(date) {
+	const hours = date.getHours().toString().padStart(2, '0')
+	const minutes = date.getMinutes().toString().padStart(2, '0')
+	return `${hours}:${minutes}`
+}
+
+function updateForecastUI(container, forecastData) {
+	for (let i = 0; i < container.length; i++) {
+		const realTime = new Date(forecastData.list[i].dt * 1000)
+		const hoursMinutes = formatTime(realTime)
+		time[i].textContent = hoursMinutes
+
+		forecastTemp[i].textContent =
+			'Temperature: ' + Math.round(forecastData.list[i].main.temp) + '°'
+		forecastFeelsLike[i].textContent =
+			'Feels like: ' + Math.round(forecastData.list[i].main.feels_like) + '°'
+
+		hourlyIcon[
+			i
+		].src = `https://openweathermap.org/img/wn/${forecastData.list[i].weather[0].icon}@4x.png`
+	}
+}
+
+async function getAndDisplayForecast(cityName) {
+	try {
+		const forecastData = await fetchWeatherData(cityName)
+		updateForecastUI(container, forecastData)
+	} catch (error) {
+		console.error('Error:', error)
 	}
 }
 
@@ -48,14 +84,16 @@ searchForm.addEventListener('submit', event => {
 		)
 		console.log(response)
 	})
-	getForecast(input.value)
-		.then(response => {
-			console.log(response)
-		})
-		.catch(error => {
-			console.log(error)
-		})
+	getAndDisplayForecast(input.value)
+	input.value = ''
 })
+
+favouriteButton.addEventListener('click', () => {
+	citiesList.push(nowCity.textContent)
+	console.log(citiesList)
+})
+
+function addCity() {}
 
 function splitFunction(city, temp, icon, feels, sunriseObj, sunsetObj) {
 	nowCity.textContent = city
